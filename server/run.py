b7098822748cfac9593ee69a7edcab6c4a7683ca
sungbin2 <sungbin222@naver.com>
2018-06-26 10:04:29 +0900

    linux1

--- server/run.py
+++ server/run.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-import werkzeug.serving
 
 sys.path.append(os.getcwd())
 
@@ -16,38 +15,8 @@ print(' PORT: ', _port)
 print('********************************************************')
 
 
-#
-#
-#
-#
-@werkzeug.serving.run_with_reloader
-def _gevent():
-    from server.main_ import app
-    from gevent.pywsgi import WSGIServer
-    WSGIServer((_host, _port), app).serve_forever()
-
-
 if framework == 'flask':
     from server.main_ import app
 
     app.run(host=_host, port=_port)
 
-
-elif framework == 'gevent':
-    _gevent()
-
-elif framework == 'cherrypy':
-    from server.main_ import app
-    import cherrypy
-
-    cherrypy.tree.graft(app, '/')
-    cherrypy.config.update(
-        {
-            'server.socket_host': _host,
-            'server.socket_port': _port,
-            'engine.autoreload.on': True,
-            'log.screen': True,
-        }
-    )
-    cherrypy.engine.start()
-    cherrypy.engine.block()
